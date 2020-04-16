93908894fe6f54f797bd5e6eae9010f27ce0d1b9
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-04-16 19:28:50 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,9 +3,5 @@ class PigLatinizer
   def initialize(word)
     @word = word
   end 
- def pig_latin(word)
-  prefix = word[0, %w(a e i o u).map{|vowel|
-    "#{word}aeiou".index(vowel)}.min]
-  "#{word[prefix.length..-1]}#{prefix}ay"
-end
+
 end 
