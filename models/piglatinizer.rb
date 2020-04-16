2155742d3b267f59de10eb9cb356284bc75244ce
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-04-16 19:26:44 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,5 +4,10 @@ class PigLatinizer
     @word = word
   end 
   
-  
+  def piglatinize(string)
+    a = string.split(" ")
+    b = a.map {|word| piglatinize_word(word)}
+    b.join(" ")
+end
+
 end 
