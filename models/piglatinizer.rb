af9ebac608cf4fed6f803ba298f17ae174c3f3da
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-04-16 19:27:13 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -8,6 +8,14 @@ class PigLatinizer
     a = string.split(" ")
     b = a.map {|word| piglatinize_word(word)}
     b.join(" ")
+  end
+  
+  def piglatinize_word(word)
+    first_letter = word[0].downcase
+    if first_letter == "a" || first_letter == "e" || first_letter == "i" || first_letter == "o" || first_letter == "u"
+        # piglatinize word that starts with a vowel
+    else
+        # piglatinize word that starts with a consonant
+    end
 end
-
 end 
